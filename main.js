c8ada50616257c66cd97bb9ebdf48f847e30a69f
Jennifer Gierka <jlgierka@gmail.com>
2020-11-01 12:08:15 -0500

    Grid container fixed, Rock Paper Scissors box added

--- main.js
+++ main.js
@@ -30,19 +30,21 @@ buttonb.onclick = function() {
   let newNumber;
   if (document.querySelector('#divide').checked == true) {
     newNumber = parseInt(inputb.value)/parseInt(inputbb.value);
-    
     para.textContent = 'The result of your numbers is: ' + newNumber;
-  } else { 
+
+  } else if (document.querySelector('#multiply').checked == true) { 
     newNumber = parseInt(inputb.value)*parseInt(inputbb.value);
-    
     para.textContent = 'The product of your numbers is: ' + newNumber;
-  }   
+  } else {
+    para.textContent = "Please select which operation to execute.";
+  }  
   boxB.appendChild(para);
       boxB.appendChild(reset);
       inputb.focus();
       reset.onclick = function() {
       boxB.removeChild(para);
-      boxB.removeChild(reset);  
+      boxB.removeChild(reset);
+      document.querySelector('input[name="multDiv"]:checked').checked = false;  
     }
   inputb.value = '';
   inputbb.value = '';
@@ -88,4 +90,64 @@ buttond.onclick = function() {
     boxD.removeChild(para);
     boxD.removeChild(reset);
   }
-}
+} 
+const boxE = document.querySelector('div.boxE');
+const buttone = document.querySelector('#e');
+const para2 = document.createElement('p');
+
+buttone.onclick = function() {
+  const playerSelection = document.querySelector('input[name=rpc]:checked').value;
+  const computerChoices = ['Rock','Paper','Scissors'];  
+  let selection = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+  
+  if (selection === playerSelection) {
+    para.textContent = selection;
+    boxE.appendChild(para);
+    para2.textContent = "Tie! Try Again."
+    boxE.appendChild(para2);
+  } 
+  else if  (selection == "Rock" && playerSelection == "Paper") {
+    para.textContent = selection;
+    boxE.appendChild(para);
+    para2.textContent = "You win! Paper beats Rock."
+    boxE.appendChild(para2);
+  }
+  else if  (selection == "Paper" && playerSelection == "Rock") {
+    para.textContent = selection;
+    boxE.appendChild(para);
+    para2.textContent = "You lose! Paper beats Rock."
+    boxE.appendChild(para2);
+  }
+  else if  (selection == "Paper" && playerSelection == "Scissors") {
+    para.textContent = selection;
+    boxE.appendChild(para);
+    para2.textContent = "You win! Scissors beats Paper."
+    boxE.appendChild(para2);
+  }
+  else if  (selection == "Rock" && playerSelection == "Scissors") {
+    para.textContent = selection;
+    boxE.appendChild(para);
+    para2.textContent = "You lose! Rock beats Scissors."
+    boxE.appendChild(para2);
+  }
+  else if  (selection == "Scissors" && playerSelection == "Paper") {
+    para.textContent = selection;
+    boxE.appendChild(para);
+    para2.textContent = "You lose! Scissors beats Paper."
+    boxE.appendChild(para2);
+  }
+  else if  (selection == "Scissors" && playerSelection == "Rock") {
+    para.textContent = selection;
+    boxE.appendChild(para);
+    para2.textContent = "You win! Rock beats Scissors."
+    boxE.appendChild(para2);
+  }
+  
+  boxE.appendChild(reset);
+  reset.onclick = function() {
+    boxE.removeChild(para);
+    boxE.removeChild(para2);
+    boxE.removeChild(reset);
+    document.querySelector('input[name="rpc"]:checked').checked = false;
+  }
+}  
